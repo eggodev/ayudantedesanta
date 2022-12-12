8014bd7912943e1a063f2d5291882fc7569aeaa2
eggodev <eggodev@gmail.com>
2022-12-11 23:31:27 -0300

    some fixes

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,29 +1,36 @@
 import crypto from "crypto";
 import getRawBody from "raw-body";
+import md5 from "md5";
 
 export default async function handler(req, res) {
-  const rawBody = await getRawBody(req);
-  const bodySignature = sha1("adams", rawBody, process.env.ADAMSPAY_API_SECRET);
+  const body = await getRawBody(req);
+  const hmacHeader = req.headers["x-adams-notify-hash"];
+  const digest = md5("adams" + body + process.env.ADAMSPAY_API_SECRET);
+  if (digest === hmacHeader) {
+    const json = JSON.parse(rawBody.toString("utf-8"));
 
-  if (bodySignature !== req.headers["x-adams-notify-hash"]) {
-    return res.status(403).json({
-      code: "invalid_signature",
-      error: "signature didn't match",
-    });
+    if (json.notify.type === "debtStatus") {
+      switch (json.debt.payStatus) {
+        case "pending":
+          console.log("email pending, include debt.payUrl");
+        case "paid":
+          console.log("email debt paid!");
+        //update debt in sanity
+        default:
+          if (
+            json.debt.objStatus.status !== "active" &&
+            json.debt.objStatus.status !== "active"
+          ) {
+            console.log("something happened, send status");
+          }
+      }
+    }
+
+    res.status(200).end("OK");
+  } else {
+    // INVALID - Respond with 401 Unauthorized
+    res.status(401).end();
   }
-
-  const json = JSON.parse(rawBody.toString("utf-8"));
-
-  switch (json.type) {
-    case "project.created":
-    // ...
-  }
-
-  res.status(200).end("OK");
-}
-
-function sha1(data, secret) {
-  return crypto.createHmac("sha1", secret).update(data).digest("hex");
 }
 
 export const config = {
