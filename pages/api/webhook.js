7ccfb59cf13facb8d03ae50b2c14663e75dcd91d
eggodev <eggodev@gmail.com>
2023-01-09 18:08:12 -0300

    webhook fixes

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -7,23 +7,40 @@ export default async function handler(req, res) {
   const hmacHeader = req.headers["x-adams-notify-hash"];
   const digest = md5("adams" + body + process.env.ADAMSPAY_API_SECRET);
   if (digest === hmacHeader) {
-    const json = JSON.parse(rawBody.toString("utf-8"));
+    const json = JSON.parse(body.toString("utf-8"));
+    console.log(json);
 
-    if (json.notify.type === "debtStatus") {
-      switch (json.debt.payStatus) {
+    if (json.notify.type === "debtStatus") {      
+      let message = "";
+      switch (json.debt.payStatus.status) {
         case "pending":
-          console.log("email pending, include debt.payUrl");
+          message = `Su pedido aun esta pendiente de pago, ingrese al siguiente enlace para finalizar su compra!: ${json.debt.payUrl}`;
+          break;
         case "paid":
-          console.log("email debt paid!");
-        //update debt in sanity
+          message = `Muchas gracias por su compra!. Ya nos disponemos a realizar el envío! Feliz Navidad! :)`;
+          //update debt in sanity
+          break;        
         default:
-          if (
-            json.debt.objStatus.status !== "active" &&
-            json.debt.objStatus.status !== "active"
-          ) {
-            console.log("something happened, send status");
-          }
+          message = `La compra no fue procesada, por favor contacte con nuestro soporte: El estado de la deuda cambió a: ${json.debt.objStatus.status}`;
       }
+      const email = json.debt.target.label;      
+      const nodemailer = require('nodemailer');
+      const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: process.env.USERMAIL,
+            pass: process.env.PASSMAIL,
+        }
+      });
+      let info = await transporter.sendMail({
+        from: 'info@ayudantedesanta.com',
+        to: email,
+        subject: "Compra realizada en ayudantedesanta.com",
+        text:message
+      })
+      .then((response)=>{console.log(response)})
+      .catch((error)=>{console.log(error)});
     }
 
     res.status(200).end("OK");
